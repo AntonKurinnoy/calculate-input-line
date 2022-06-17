9f0aa79549c1a6b7bffe12280a0a599e71e9c5a2
AntonKurinnoy <anton.kurinnoy@gmail.com>
2022-06-17 16:48:33 +0300

    Add input check for correct line

--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -2,79 +2,84 @@ fun main() {
     println("Enter line")
     val line = readLine()
     var res = line
-    var cs = res as CharSequence
-    var matchResult = Regex("""((\d+)*[.]?(\d+))([/,*]((\d+)*[.]?(\d+)))+""").findAll(cs)
-    for (element in matchResult){
 
-        val numbers = arrayListOf<Double>()
-        Regex("""\d+[.]?\d?""").findAll(element.value).forEach { it ->
-            numbers.add(it.value.toDouble())
-        }
+    val regExpRes = line?.matches(Regex("""(^\d+)([-+/*]\d+)+"""))
+    if (regExpRes === true){
+        var cs = res as CharSequence
+        var matchResult = Regex("""((\d+)*[.]?(\d+))([/,*]((\d+)*[.]?(\d+)))+""").findAll(cs)
+        for (element in matchResult){
 
-        val signs = arrayListOf<String>()
-        Regex("""(/|[*])""").findAll(element.value).forEach { it ->
-            signs.add(it.value)
-        }
-
-        var sum = 0.00
-        signs.forEachIndexed { index, el ->
-
-            if (index < 1){
-                if (signs[index] == "*") {
-                    sum = numbers[index] * numbers[1]
-                } else {
-                    sum = numbers[index] / numbers[1]
-                }
-            } else {
-                if (signs[index] == "*") {
-                    sum *= numbers[index + 1].toDouble()
-                } else {
-                    if(numbers[index + 1].toDouble() > 0){
-                        sum /= numbers[index + 1].toDouble()
-                    } else throw Exception("Division by zero!")
-
-                }
+            val numbers = arrayListOf<Double>()
+            Regex("""\d+[.]?\d?""").findAll(element.value).forEach { it ->
+                numbers.add(it.value.toDouble())
             }
 
-        }
-        res = res?.replaceFirst(element.value, sum.toString())
-        cs = res as CharSequence
+            val signs = arrayListOf<String>()
+            Regex("""(/|[*])""").findAll(element.value).forEach { it ->
+                signs.add(it.value)
+            }
 
-    }
+            var sum = 0.00
+            signs.forEachIndexed { index, el ->
 
-    matchResult = Regex("""((\d+)*[.]?(\d+))([+,-]((\d+)*[.]?(\d+)))+""").findAll(cs)
-    for (element in matchResult){
-        val numbers = arrayListOf<Double>()
-        Regex("""\d+[.]?\d?""").findAll(element.value).forEach { it ->
-            numbers.add(it.value.toDouble())
-        }
-
-        val signs = arrayListOf<String>()
-        Regex("""([+]|-)""").findAll(element.value).forEach { it ->
-            signs.add(it.value)
-        }
-
-        var sum = 0.00
-        signs.forEachIndexed { index, el ->
-
-            if (index < 1){
-                if (signs[index] == "+") {
-                    sum = numbers[index] + numbers[1]
+                if (index < 1){
+                    if (signs[index] == "*") {
+                        sum = numbers[index] * numbers[1]
+                    } else {
+                        sum = numbers[index] / numbers[1]
+                    }
                 } else {
-                    sum = numbers[index] - numbers[1]
+                    if (signs[index] == "*") {
+                        sum *= numbers[index + 1].toDouble()
+                    } else {
+                        if(numbers[index + 1].toDouble() > 0){
+                            sum /= numbers[index + 1].toDouble()
+                        } else throw Exception("Division by zero!")
+
+                    }
                 }
-            } else {
-                if (signs[index] == "+") {
-                    sum += numbers[index + 1].toDouble()
+
+            }
+            res = res?.replaceFirst(element.value, sum.toString())
+            cs = res as CharSequence
+
+        }
+
+        matchResult = Regex("""((\d+)*[.]?(\d+))([+,-]((\d+)*[.]?(\d+)))+""").findAll(cs)
+        for (element in matchResult){
+            val numbers = arrayListOf<Double>()
+            Regex("""\d+[.]?\d?""").findAll(element.value).forEach { it ->
+                numbers.add(it.value.toDouble())
+            }
+
+            val signs = arrayListOf<String>()
+            Regex("""([+]|-)""").findAll(element.value).forEach { it ->
+                signs.add(it.value)
+            }
+
+            var sum = 0.00
+            signs.forEachIndexed { index, el ->
+
+                if (index < 1){
+                    if (signs[index] == "+") {
+                        sum = numbers[index] + numbers[1]
+                    } else {
+                        sum = numbers[index] - numbers[1]
+                    }
                 } else {
-                    sum -= numbers[index + 1].toDouble()
+                    if (signs[index] == "+") {
+                        sum += numbers[index + 1].toDouble()
+                    } else {
+                        sum -= numbers[index + 1].toDouble()
+                    }
                 }
             }
+            res = res?.replaceFirst(element.value, sum.toString())
+            cs = res as CharSequence
         }
-        res = res?.replaceFirst(element.value, sum.toString())
-        cs = res as CharSequence
+
+        println(cs)
+    } else {
+        println("Wrong line")
     }
-
-    println(cs)
-
 }
