516a002445e7301c830ad78781a51c04f66b32a4
AntonKurinnoy <anton.kurinnoy@gmail.com>
2022-06-29 12:58:52 +0300

    add evaluate fun to ExpressionController

--- src/main/kotlin/org/test/Dao.kt
+++ src/main/kotlin/org/test/Dao.kt
@@ -1,8 +1,5 @@
 package org.test
 
-/**
- * @author Slava Gornostal
- */
 data class Request(val expression: String)
 
 data class Response(val result: Double)
--- src/main/kotlin/org/test/ExpressionController.kt
+++ src/main/kotlin/org/test/ExpressionController.kt
@@ -1,19 +1,14 @@
 package org.test
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
-/**
- * @author Slava Gornostal
- */
 @RestController
 @RequestMapping("/api")
 class ExpressionController {
 
     @PostMapping("/calculate-expression")
-    suspend fun test(request: Request): Response {
-        TODO()
+    suspend fun evaluate(@RequestBody request: Request): Response {
+        return Response(SimpleExpressionEvaluator().evaluate(request.expression))
     }
 
 }
